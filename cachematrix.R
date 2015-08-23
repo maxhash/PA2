23a1eef72cd6d9af536a5e6a6b75702d9dd971d6
Max Hvoshch <ukerest@gmail.com>
2015-08-23 20:38:07 +0100

    1st commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix uses scoping rules and stores matrices in memory
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(X = matrix()) {
+inverse <- NULL
+set <- function(Y){
+	X <<- Y
+	inverse <<- NULL
+	}
+get <- function() X
+setinverse <- function(Inverse) inverse <<- Inverse
+getinverse <- function() inverse
+list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve uses corpcor, a library that avoids determinants and uses orthogonal decomposition
+## this function will corpcor library and if it's not installed will install the library
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(X, ...) 
+{
+if(require("corpcor")){
+	print("corpcor is loaded")
+	} else {
+		print("installing corpcor")
+		install.packages("corpcor")
+		if(require(corpcor)){
+			print("corpcor installed")
+			} else {
+			stop("corpcor installation failed")
+			}
+		}
+inverse <- X$getinverse()
+if(!is.null(inverse)){
+	message("matrix is in memory")
+	return(inverse)
+	}
+message("inverse is being computed")
+data <- X$get()
+inverse <- pseudoinverse(data, ...)
+X$setinverse(inverse)
+inverse
 }
+
